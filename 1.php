24def928a7d59684eb565e9474c0c9c4259d1644
danilaka <danilaka25@gmail.com>
2018-09-09 14:31:40 +0300

    First Commit

---
+++ 1.php
@@ -0,0 +1,35 @@
+<html>
+ 
+<head>
+ 
+  <link rel="stylesheet" type="text/css" href="css/styles.css">
+ 
+  <title></title>
+ 
+</head>
+ 
+<body>
+ 
+<h1>I'm working on a PHP project using the Zend framework which is being served locally using Apache. Is there a plug-in/configuration for Grunt/Gulp t</h1>
+
+<div>23123</div>
+
+
+
+<div class="home">
+	lorem input
+</div>
+
+
+<form action="/action_page.php">
+  <input type="text" name="fname" placeholder="First name"><br>
+  <input type="text" name="lname" placeholder="Last name"><br>
+  <input type="submit" value="Submit">
+</form> 
+
+ 
+<script type="text/javascript" src="js/theme.js"></script>
+ 
+</body>
+ 
+</html>
