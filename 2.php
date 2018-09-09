5416362be92ed22284435e5d35a94f3f5a674805
danilaka <danilaka25@gmail.com>
2018-09-09 19:00:55 +0300

    2 commit

---
+++ 2.php
@@ -0,0 +1,22 @@
+<html>
+ 
+<head>
+ 
+  <link rel="stylesheet" type="text/css" href="css/styles.css">
+ 
+  <title></title>
+ 
+</head>
+ 
+<body>
+ 
+<h1>77777</h1>
+
+<div>666</div>
+<?php echo "3432443"; ?>
+ 
+<script type="text/javascript" src="js/theme.js"></script>
+ 
+</body>
+ 
+</html>
